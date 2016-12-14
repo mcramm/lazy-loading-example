0fc4038b0806d1f0f7f13228fee46456892628a9
Michael Cramm <mike@mcramm.com>
2016-12-14 08:24:36 -0700

    Migrate test db too

--- dev/user.clj
+++ dev/user.clj
@@ -9,11 +9,17 @@
   {:datastore (ragtime.jdbc/sql-database (env :database-url))
    :migrations (ragtime.jdbc/load-resources "migrations")})
 
+(def test-config
+  {:datastore (ragtime.jdbc/sql-database (str (env :database-url) "_test"))
+   :migrations (ragtime.jdbc/load-resources "migrations")})
+
 (defn migrate []
-  (ragtime.repl/migrate config))
+  (ragtime.repl/migrate config)
+  (ragtime.repl/migrate test-config))
 
 (defn rollback []
-  (ragtime.repl/rollback config))
+  (ragtime.repl/rollback config)
+  (ragtime.repl/rollback test-config))
 
 (defn create-store []
   (postgres/build (env :database-url)))
